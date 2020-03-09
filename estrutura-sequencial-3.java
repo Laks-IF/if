050d6b8d2ecb3b4f7d45d9e2d0842b6635518b7d
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-09 16:52:11 -0400

    Update estrutura-sequencial-3.java

--- estrutura-sequencial-3.java
+++ estrutura-sequencial-3.java
@@ -1,3 +1,4 @@
+// EXERCÍCIO 1 =====================================================================================================
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -25,5 +26,40 @@ public class JavaApplication1 {
         
         int sum = num1 + num2;
         System.out.println("SOMA = " + sum);
+    }
+}
+
+
+// EXERCÍCIO 2 ===================================================================================================================
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 06792799114
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        double radius = scan.nextDouble();
+        double area = Math.pow(radius, 2) * Math.PI;
+        
+        DecimalFormat df = new DecimalFormat("0.####");
+        System.out.println("A=" + df.format(area));
+        
+        
     }
 }
