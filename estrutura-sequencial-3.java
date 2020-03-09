57f6c8f3d95a642836392ce9ac081e528a0e58f0
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-09 17:17:12 -0400

    Update estrutura-sequencial-3.java

--- estrutura-sequencial-3.java
+++ estrutura-sequencial-3.java
@@ -139,3 +139,51 @@ public class JavaApplication1 {
         System.out.println("SALARY = U$ " + salary);
     }
 }
+
+
+// EXERCÍCIO 5 ========================================================================================================================
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 06792799114
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
+        String peca1Data = scan.next();
+        String peca2Data = scan.next();
+        
+        String[] peca1 = peca1Data.split(" ");
+        
+        System.out.println(peca1[1]);
+        /*
+        int cod = scan.nextInt();
+        double valorPeca = scan.nextDouble();
+        int qtdPecas = scan.nextInt();
+
+        double subTotal = valorPeca * qtdPecas;
+
+        DecimalFormat df = new DecimalFormat("0.##");
+
+        System.out.println("VALOR A PAGAR = U$ " + df.format(subTotal));
+        */
+    }
+}
